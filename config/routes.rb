bca0b44a3698b9ea54c6f29eeef65182f189f829
doyadoya213 <213enjoy.yolo@gmail.com>
2022-05-06 12:53:48 +0900

    [Modify] ・Deviseのルーティングをカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+    get 'login', to: 'devise/sessions#new'
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
 end
