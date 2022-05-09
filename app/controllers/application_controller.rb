0f561bbdca98d7372e4a685b2272d3a3171f0ee0
doyadoya213 <213enjoy.yolo@gmail.com>
2022-05-09 11:36:18 +0900

    [Add] ・エラー画面追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  unless Rails.env.development?
+    rescue_from StandardError, with: :render_500
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  end
+
+  private
+
+  def render_404
+    render template: 'errors/404', status: 404
+  end
+
+  def render_500
+    render template: 'errors/500', status: 500
+  end
 end
