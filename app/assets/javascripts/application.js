c71ae06274360ec94b97b9e41871f1a9a34590bd
doyadoya213 <213enjoy.yolo@gmail.com>
2022-05-11 13:51:41 +0900

    [Add] ・いいね機能追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
 //= require rails-ujs
-//= require jquery
-//= require jquery_ujs
 //= require bootstrap-material-design/dist/js/bootstrap-material-design.js
 //= require swiper/swiper-bundle.js
 //= require swiper.js
