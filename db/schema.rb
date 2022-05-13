61920dfeff41597a038817cef9b97d7bba151c36
doyadoya213 <213enjoy.yolo@gmail.com>
2022-05-13 17:09:10 +0900

    [Add] ・Userにusername追加 ・ユーザー詳細ページに投稿一覧追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_12_004218) do
+ActiveRecord::Schema.define(version: 2022_05_13_063141) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.text "body", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2022_05_12_004218) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,8 +1,9 @@
 puts 'Start inserting user seeds ...'
 
-User.create!(email: 'test001@example.com', password: 'password')
+User.create!(username: 'test user001', email: 'test001@example.com', password: 'password')
 10.times do
   User.create!(
+    username: Faker::Internet.unique.user_name,
     email: Faker::Internet.email,
     password: 'password'
   )
